279ef1d43ddc49fa52758b43c9bde2f4930519b0
ededyyy <148532242+ededyyy@users.noreply.github.com>
2024-05-30 14:37:56 +0800

    Update mazegen.c
    success program

--- mazegen.c
+++ mazegen.c
@@ -6,6 +6,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
+#include <ctype.h>
+
+#define PATH 0
+#define WALL 1
 typedef struct __Coord
 {
     int x;
@@ -14,33 +19,30 @@ typedef struct __Coord
 
 typedef struct __Maze
 {
-    char **map;
+    int **map;
     int height;
     int width;
     coord start;
     coord end;
 } maze;
 
+int rank = 0;
+
 /**
- * @brief Initialise a maze object
+ * @brief Check if the str is integer
  *
- * @param maze to be initialised
- * @param height height to allocate
- * @param width width to allocate
- * @return maze
+ * @param str pointer
+ * @return 1 fail, 0 success
  */
-maze initial_maze(maze maze, int height, int width) {
-	maze.height = height;
-	maze.width = width;
-	maze.map = (char**)malloc(sizeof(char*)*height);
-	int i, j;
-	for (i = 0; i < height; i++) {
-		maze.map[i] = (char*)malloc((width+1)*sizeof(char));
-		for (j = 0;j < width; j++) {
-			maze.map[i][j] = '#';
+int is_int(char *str) {
+	if (str == NULL) return 1;
+	while (*str != '\0') {
+		if(isdigit(*str) == 0) {
+			return 1;
 		}
+		str++;
 	}
-	return maze;
+	return 0;
 }
 /**
  * @brief Set start point for maze
@@ -50,8 +52,8 @@ maze initial_maze(maze maze, int height, int width) {
  */
 maze set_start(maze instance) {
 	srand((unsigned)time(NULL));
-	int i = rand() % (instance.height);
-	int j = rand() % (instance.width);
+	int i = rand() % (instance.height - 2) + 1;
+	int j = rand() % (instance.width - 2) + 1;
 	instance.start.x = j;
 	instance.start.y = i;
 	return instance;
@@ -66,22 +68,106 @@ maze set_start(maze instance) {
 maze set_end(maze instance) {
 	srand((unsigned)time(NULL));
 	do {
-		int i = rand() % (instance.height);
-	    int j = rand() % (instance.width);
+		int i = rand() % (instance.height - 2) + 1;
+	    int j = rand() % (instance.width - 2 ) + 1;
 	    instance.end.x = j;
 	    instance.end.y = i;
 	} while (instance.start.x == instance.end.x && instance.start.y == instance.end.y);
 	return instance;
 }
+
+/**
+ * @brief Initialise a maze object
+ *
+ * @param maze to be initialised
+ * @param height height to allocate
+ * @param width width to allocate
+ * @return maze
+ */
+maze initial_maze(maze maze, int height, int width) {
+	maze.height = height;
+	maze.width = width;
+	maze.map = (int**)malloc(sizeof(int*)*height);
+	int i, j;
+	for (i = 0; i < height; i++) {
+		maze.map[i] = (int*)malloc((width+1)*sizeof(int));
+	}
+	for (i = 1; i < (height - 1); i++) {
+		for (j = 1;j < (width - 1); j++) {
+			maze.map[i][j] = WALL;
+		}
+	}
+	for (j = 0; j < width; j++) {
+		maze.map[0][j] = PATH;
+		maze.map[height - 1][j] = PATH;
+	}
+	for (i = 0; i < height; i++) {
+		maze.map[i][0] = PATH;
+		maze.map[i][width - 1] = PATH;
+	}
+	maze = set_start(maze);
+	maze = set_end(maze);
+	maze.map[maze.end.x][maze.end.y] = PATH;
+	return maze;
+}
+
 /**
  * @brief Generate a maze object
  *
  * @param maze to be initialised
+ * @param int x
+ * @param int y
  * @return maze
  */
-maze generate_maze(maze instance) {
-	instance = set_start(instance);
-	instance = set_end(instance);
+maze generate_maze(maze instance, int x, int y) {
+	// Select directions randomly
+	srand((unsigned)time(NULL));
+	instance.map[x][y] = PATH;
+	int direction[4][2] = { { 1,0 },{ -1,0 },{ 0,1 },{ 0,-1 } };
+	int i;
+	for (i = 0; i < 4; i++) {
+		int r = rand() % 4;
+		int temp = direction[0][0];
+		direction[0][0] = direction[r][0];
+		direction[r][0] = temp;
+		temp = direction[0][1];
+		direction[0][1] = direction[r][1];
+		direction[r][1] = temp;
+    }
+    for (i = 0; i < 4; i++) {
+    	int dx = x;
+    	int dy = y;
+    	int range;
+    	if (rank == 0) range = 1;
+    	else range = rand() % rank + 1;
+    	while (range > 0) {
+    		dx += direction[i][0];
+    		dy += direction[i][1];
+    		// if there exists path already exists, break
+			if (instance.map[dx][dy] == PATH) {
+    			break;
+			}
+			// if paths meet, break
+			int count = 0;
+			int j, k;
+			for (j = dx - 1; j < dx + 2; j++) {
+				for (k = dy - 1; k < dy + 2; k++) {
+					if (abs(j - dx) + abs(k - dy) == 1 && instance.map[j][k] == PATH) {
+						count++;
+					}
+				}
+			}
+			if (count > 1) {
+				break;
+			}
+			--range;
+			instance.map[dx][dy] = PATH;	
+		}
+		// recursive
+		if (range <= 0) {
+			generate_maze(instance, dx, dy);
+		}
+	}
 	return instance;
 }
 
@@ -92,7 +178,6 @@ maze generate_maze(maze instance) {
  * @param maze object
  */
 void print_maze(FILE* p, maze instance) {
-	instance = generate_maze(instance);
 	int i, j;
 	for (i = 0; i < instance.height; i++)
     {
@@ -102,8 +187,11 @@ void print_maze(FILE* p, maze instance) {
 			} else if (i == instance.end.y && j == instance.end.x) {
 				fprintf(p, "%c", 'E');
 			}
+			else if (instance.map[i][j] == PATH) {
+				fprintf(p, "%c", ' ');
+			}
 			else {
-				fprintf(p, "%c", instance.map[i][j]);
+				fprintf(p, "%c", '#');
 			}
 		}
 		fprintf(p, "\n");
@@ -112,6 +200,10 @@ void print_maze(FILE* p, maze instance) {
 
 int main(int argc, char *argv[]) {
 	if (argc != 4) {
+		printf("Argument error");
+		return 1;
+	}
+	if (is_int(argv[2]) == 1 || is_int(argv[3]) == 1) {
 		printf("Argument error");
 		return 1;
 	}
@@ -129,9 +221,14 @@ int main(int argc, char *argv[]) {
 	}
 	maze instance;
 	instance = initial_maze(instance, height, width);
-	instance = generate_maze(instance);
+	instance = generate_maze(instance, instance.start.x, instance.start.y);
 	print_maze(p, instance);
 	fclose(p);
-	printf("success");	
+	printf("success");
+	int i;
+	for (i = 0; i < instance.height; i++) {
+		free(instance.map[i]);
+	}
+	free(instance.map);
 	return 0;
 } 
